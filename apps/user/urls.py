7cb7f269b66e13a39399239897c3e2156f2a6e66
JiushixihuanAn <464432965@qq.com>
2020-03-24 23:16:40 +0800

    2 导入静态文件

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,8 @@
 
 
-from django.urls import path
+from django.urls import path,re_path
+from apps.user import views
 
 urlpatterns = [
-
+    re_path('^register',views.register,name='register')
 ]
--- dailyfresh/urls.py
+++ dailyfresh/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'^tinymce',include('tinymce.urls')),
-    path(r'^user/',include('apps.user.urls')),
-    path(r'^cart/',include('apps.cart.urls')),
-    path(r'^order/',include('apps.order.urls')),
+    re_path('^user/',include('apps.user.urls')),
+    path(r'^cart',include('apps.cart.urls')),
+    path(r'^order',include('apps.order.urls')),
     path(r'^',include('apps.goods.urls')),
 ]
